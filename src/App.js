ed6cd8daf89582dc13c3bfcc0dbe6e20f6f1f6b5
avorona <vitalii.parshikov@gmail.com>
2018-01-31 21:58:40 +0200

    Init ICO table app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ICOTable from './components/containers/ICOTable'
+
+class App extends Component {
+  render() {
+    return ( 
+      <ICOTable/>
+    );
+  }
+}
+
+export default App;
+
+
+// https://www.cryptocompare.com/api/data/coinlist/
---
+++ src/components/containers/ICOTable.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+
+
+
+
+class ICOTable extends Component {
+
+  constructor(props) {
+    this.state={
+      
+    }
+
+  }
+render() {
+
+  return (
+
+      
+  
+
+  );
+}
+
+}
+
+export default ICOTable;
