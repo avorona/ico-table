931cd533afab088ce07b7333c2d9c45ba49f6b4b
avorona <vitalii.parshikov@gmail.com>
2018-02-02 23:58:44 +0200

    Add react-table to deps and clean up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+
 import ICOTable from './components/containers/ICOTable'
+import './App.css'
 
 class App extends Component {
   render() {
     return ( 
-      <ICOTable/>
+        <div className="App"> 
+          <ICOTable length={30} /> 
+        </div>     
     );
   }
 }
 
 export default App;
-
-
-// https://www.cryptocompare.com/api/data/coinlist/
